ba25df59606ae6865e59a9e696190060ae679c74
darilag2 <daryl_darilag@yahoo.co.uk>
2021-01-16 07:12:17 +0000

    home page updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../App.css'
 
 export default function Main() {
     return (
         <div className="home">
-            <h1>Welcome to Hype Review</h1>
+            <h1>Hype Review</h1>
+            <Link to="/signin"><button className="btn">Get Started</button></Link>
         </div>
     )
 }
