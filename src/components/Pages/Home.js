e24cd76035a2c1e430b164ae137b23b821a7d149
darilag2 <daryl_darilag@yahoo.co.uk>
2021-01-16 04:42:22 +0000

    footer

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Main from '../Main'
 import '../../App.css'
+import Footer from '../Footer'
 
 export default function Home() {
     return (
         <>
         <Main />
+        <Footer />
         </>
     )
 }
