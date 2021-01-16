8781c99fb2298fbff199178bf9389d4f6c2c4eb2
darilag2 <daryl_darilag@yahoo.co.uk>
2021-01-16 04:34:44 +0000

    logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ export default function NavBar() {
 return (
     <nav className="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
-            <Link to="/" className="navbar-brand">Hype Review</Link>
+            <Link to="/" className="navbar-brand"><h1>Hype Review<i className="fas fa-expand-arrows-alt"></i></h1></Link>
             <div class="d-flex" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <Link to="/" className="nav-link active">Home</Link>
+                        <Link to="/" className="nav-links">Home</Link>
                     </li>
                     <li class="nav-item">
                         <Link to="/library" className="nav-links">Library</Link>
@@ -19,7 +19,7 @@ return (
                         <Link to="/hype" className="nav-links">Hype</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to="/signin" className="nav-links">SignIn</Link>
+                        <Link to="/signin" className="nav-links-button">SignIn</Link>
                     </li>
                 </ul>
             </div>
