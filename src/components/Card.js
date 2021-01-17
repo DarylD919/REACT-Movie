d74a7a0c9461d776953d1b072fd1ea67bf98f516
darilag2 <daryl_darilag@yahoo.co.uk>
2021-01-17 23:47:42 +0000

    card layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,43 @@
 import React from 'react'
 
+
 export default function Card() {
     return (
-        <div className="card">
-            <p>Action</p>
-            <p>Romance</p>
-            <p>Adventure</p>
-            <p>Horror</p>
-            <p>Zombies</p>
+        <div class="card-group">
+            <div class="card text-center">
+                <div class="img-1" />
+                <div class="card-body">
+                    <h5 class="card-title">Action</h5>
+                    <p class="card-text">Action packed </p>
+                    <a href="/" class="btn btn-primary">Library</a>
+                </div>
+            </div>
+            <div class="card text-center">
+                <div class="img-2" />
+                <div class="card-body">
+                    <h5 class="card-title">Romance</h5>
+                    <p class="card-text">Need a little love</p>
+                    <a href="/" class="btn btn-primary">Library</a>
+                </div>
+            </div>
+            <div class="card text-center">
+            <div class="img-3" />
+                <div class="card-body">
+                    <h5 class="card-title">Horror</h5>
+                    <p class="card-text">Prepare to be scared</p>
+                    <a href="/" class="btn btn-primary">Library</a>
+                </div>
+            </div>
+            <div class="card text-center">
+                <div class="img-4" />
+                <div class="card-body">
+                    <h5 class="card-title">Adventure</h5>
+                    <p class="card-text">Adventure awaits</p>
+                    <a href="/" class="btn btn-primary">Library</a>
+                </div>
+            </div>
+            
+
         </div>
     )
 }
--- src/components/Pages/Library.js
+++ src/components/Pages/Library.js
@@ -4,7 +4,7 @@ import Card from '../Card'
 export default function Library() {
     return (
         <div className="library">
-            <h1>Card system for the library</h1>
+            <h1>library</h1>
             <Card />
         </div>
     )
